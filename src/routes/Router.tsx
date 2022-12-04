e8ae46064b843838e54e1684ee8e22b61c0c24db
yong <tkddbs1644@gmail.com>
2022-12-04 19:28:35 +0900

    fix(Router): BrouserRouter를 Router.tsx로 위치 변경

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,17 +1,19 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { CheckNotePage, HomePage, NotFoundPage, QuizPage, ResultPage } from "~/pages";
 import { ROUTE_PATHS } from "./paths";
 
 const Router = () => {
   return (
-    <Routes>
-      <Route path={ROUTE_PATHS.HOME.path} element={<HomePage />} />
-      <Route path={ROUTE_PATHS.QUIZ.path} element={<QuizPage />} />
-      <Route path={ROUTE_PATHS.RESULT.path} element={<ResultPage />} />
-      <Route path={ROUTE_PATHS.CHECK_NOTE.path} element={<CheckNotePage />} />
-      <Route path={ROUTE_PATHS.NOT_FOUND.path} element={<NotFoundPage />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path={ROUTE_PATHS.HOME.path} element={<HomePage />} />
+        <Route path={ROUTE_PATHS.QUIZ.path} element={<QuizPage />} />
+        <Route path={ROUTE_PATHS.RESULT.path} element={<ResultPage />} />
+        <Route path={ROUTE_PATHS.CHECK_NOTE.path} element={<CheckNotePage />} />
+        <Route path={ROUTE_PATHS.NOT_FOUND.path} element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
