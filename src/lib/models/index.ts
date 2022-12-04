32744696674809595f272d435ca58dccc400880e
yong <tkddbs1644@gmail.com>
2022-12-05 02:35:37 +0900

    feat(Api): Quiz Api 기능 구현

---
+++ src/lib/models/index.ts
@@ -0,0 +1,10 @@
+export type QuizType = "multiple";
+
+export type IQuiz = {
+  category: string;
+  type: QuizType;
+  difficulty: "easy" | "medium" | "hard";
+  question: string;
+  correct_answer: string;
+  incorrect_answers: [string, string, string];
+};
---
+++ src/lib/services/quiz.ts
@@ -0,0 +1,28 @@
+import Api from "../core/Api";
+import { IQuiz, QuizType } from "../models";
+
+type ResponseData = {
+  response_code: number;
+  results: IQuiz[];
+};
+
+function makeQuery({ amount, type }: { amount: number; type: QuizType }) {
+  return { amount, type };
+}
+
+class QuizApi extends Api {
+  async getQuizzes(): Promise<ResponseData> {
+    const query = makeQuery({ amount: 10, type: "multiple" });
+    const response = await this.baseRequest.get("", {
+      params: query,
+    });
+    if (response.data && response.data.response_code !== 0) {
+      throw new Error("data is nothing. Probably url is wrong.");
+    }
+    return response.data;
+  }
+}
+
+const quizApi = new QuizApi();
+
+export default quizApi;
