1f13bdef686459b973583b2c99337fe30c11bea6
yong <tkddbs1644@gmail.com>
2022-12-06 19:45:43 +0900

    feat(store/useQuiz): quiz 비즈니스 로직을 담당하는 커스텀 훅 구현

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { shallowEqual, useDispatch, useSelector } from "react-redux";
-import quizReducer from "./quiz";
+import quizReducer from "./quiz/slice";
+
+export function useShallowSelector<T>(fn: (state: RootState) => T): T {
+  return useSelector(fn, shallowEqual);
+}
 
 const store = configureStore({
   reducer: {
@@ -8,14 +12,10 @@ const store = configureStore({
   },
 });
 
-type QuizDispatch = typeof store.dispatch;
-
 export type RootState = ReturnType<typeof store.getState>;
 
-export const useQuizDispatch = () => useDispatch<QuizDispatch>();
+type QuizDispatch = typeof store.dispatch;
 
-export function useShallowSelector<T>(fn: (state: RootState) => T): T {
-  return useSelector(fn, shallowEqual);
-}
+export const useQuizDispatch = () => useDispatch<QuizDispatch>();
 
 export default store;
---
+++ src/store/useQuiz.tsx
@@ -0,0 +1,71 @@
+import { useCallback, useMemo } from "react";
+import { IQuiz } from "~/lib/models";
+import { useQuizDispatch, useShallowSelector } from ".";
+import { quizActions } from "./quiz/slice";
+
+const useQuiz = () => {
+  const quizState = useShallowSelector((state) => state.quiz);
+  const dispatch = useQuizDispatch();
+
+  const isFinished = useMemo(
+    () => quizState.currentQuizIndex >= quizState.quizzes.length,
+    [quizState.currentQuizIndex, quizState.quizzes],
+  );
+
+  const wrongQuizzesCount = useMemo(() => {
+    return quizState.wrongQuizIndexNumbers.length;
+  }, [quizState.wrongQuizIndexNumbers]);
+
+  const correctQuizzesCount = useMemo(() => {
+    return quizState.quizzes.length - wrongQuizzesCount;
+  }, [quizState.quizzes, wrongQuizzesCount]);
+
+  const wrongQuizzes = useMemo(() => {
+    return quizState.wrongQuizIndexNumbers.map((indexNumber) => quizState.quizzes[indexNumber]);
+  }, [quizState.wrongQuizIndexNumbers, quizState.quizzes]);
+
+  const isCorrect = useCallback(
+    (selectedAnswer: string) => {
+      const currentQuiz = quizState.quizzes[quizState.currentQuizIndex];
+      return currentQuiz.correct_answer === selectedAnswer;
+    },
+    [quizState.quizzes, quizState.currentQuizIndex],
+  );
+
+  const setQuizzes = useCallback(
+    (newQuizzes: IQuiz[]) => {
+      dispatch(quizActions.setQuizzes(newQuizzes));
+    },
+    [dispatch],
+  );
+
+  const selectAnswer = useCallback(
+    (isAnswered: boolean) => {
+      dispatch(quizActions.selectAnswer(isAnswered));
+    },
+    [dispatch],
+  );
+
+  const goPrevQuiz = useCallback(() => {
+    dispatch(quizActions.gePrevQuiz());
+  }, [dispatch]);
+
+  const goNextQuiz = useCallback(() => {
+    dispatch(quizActions.goNextQuiz());
+  }, [dispatch]);
+
+  return {
+    ...quizState,
+    isFinished,
+    wrongQuizzesCount,
+    correctQuizzesCount,
+    wrongQuizzes,
+    isCorrect,
+    setQuizzes,
+    selectAnswer,
+    goPrevQuiz,
+    goNextQuiz,
+  };
+};
+
+export default useQuiz;
