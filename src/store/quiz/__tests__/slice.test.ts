5f40dbcc91dbb69dff603be2dcf68eadf5d295fb
yong <tkddbs1644@gmail.com>
2022-12-06 21:55:50 +0900

    test(store/quiz): goNextQuiz() 테스트 코드 추가

--- src/store/quiz/__tests__/slice.test.ts
+++ src/store/quiz/__tests__/slice.test.ts
@@ -120,7 +120,7 @@ describe("store/quiz 리듀서 단위 테스트", () => {
       currentQuizIndex: 0,
     };
 
-    expect(reducer(prevState, quizActions.gePrevQuiz())).toEqual(result);
+    expect(reducer(prevState, quizActions.goPrevQuiz())).toEqual(result);
   });
 
   test("2페이지 이상에서 이전 페이지를 누르면 이전 페이지로 간다.", () => {
@@ -135,6 +135,39 @@ describe("store/quiz 리듀서 단위 테스트", () => {
       currentQuizIndex: 0,
     };
 
-    expect(reducer(prevState, quizActions.gePrevQuiz())).toEqual(result);
+    expect(reducer(prevState, quizActions.goPrevQuiz())).toEqual(result);
+  });
+
+  test("다음 페이지로 간다.", () => {
+    const prevState: QuizState = {
+      quizzes: [...mockQuizzes],
+      currentQuizIndex: 0,
+      wrongQuizIndexNumbers: [],
+    };
+
+    const result: QuizState = {
+      ...prevState,
+      currentQuizIndex: 1,
+    };
+
+    expect(reducer(prevState, quizActions.goNextQuiz())).toEqual(result);
+  });
+
+  test("마지막 페이지에서 다음 페이지를 계속 눌러도 currentQuizIndex는 퀴즈 배열의 길이이다.", () => {
+    const prevState: QuizState = {
+      quizzes: [...mockQuizzes],
+      currentQuizIndex: 1,
+      wrongQuizIndexNumbers: [],
+    };
+
+    reducer(prevState, quizActions.goNextQuiz());
+    reducer(prevState, quizActions.goNextQuiz());
+
+    const result: QuizState = {
+      ...prevState,
+      currentQuizIndex: 2,
+    };
+
+    expect(reducer(prevState, quizActions.goNextQuiz())).toEqual(result);
   });
 });
