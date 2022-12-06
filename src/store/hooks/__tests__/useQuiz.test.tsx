02f40aa90365af43bb1b2c61a75df748b2de0efd
yong <tkddbs1644@gmail.com>
2022-12-07 01:45:42 +0900

    test(useQuiz): useQuiz 기능 테스트 코드 추가

--- src/store/hooks/__tests__/useQuiz.test.tsx
+++ src/store/hooks/__tests__/useQuiz.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "@testing-library/jest-dom";
 import { mockQuizzes } from "~/test-utils/mock";
-import { act, renderHook } from "@testing-library/react-hooks";
+import { act, renderHook, RenderResult } from "@testing-library/react-hooks";
 import { Provider } from "react-redux";
 import store from "~/store";
 import useQuiz from "../useQuiz";
@@ -10,16 +10,113 @@ const wrapper = ({ children }: { children: React.ReactNode }) => (
   <Provider store={store}>{children}</Provider>
 );
 
+const makeRandomUserAnswers = (length: number) => {
+  const makeRandomBoolean = () => Math.random() * 10 < 5;
+  const userAnswers = Array.from({ length }, () => makeRandomBoolean());
+  const correctCount = userAnswers.filter((answer) => answer === true).length;
+  const wrongCount = length - correctCount;
+  return { userAnswers, correctCount, wrongCount };
+};
+
+const getSortedWrongQuizzes = (userAnswers: boolean[]) => {
+  const wrongQuizzesResult = [];
+  for (let index = 0; index < mockQuizzes.length; index += 1) {
+    if (!userAnswers[index]) {
+      wrongQuizzesResult.push(mockQuizzes[index]);
+    }
+  }
+  return wrongQuizzesResult;
+};
+
+const getQuestions = () => {
+  return mockQuizzes.map((quiz) => ({
+    correctAnswer: quiz.correct_answer,
+    wrongAnswers: [...quiz.incorrect_answers],
+  }));
+};
+
+const startQuiz = (result: RenderResult<ReturnType<typeof useQuiz>>) => {
+  act(() => {
+    result.current.setNewQuizzes(mockQuizzes);
+  });
+};
+
 describe("useQuiz 기능 테스트", () => {
-  beforeEach(() => {
+  test("새로운 퀴즈를 시작한다.", () => {
     const { result } = renderHook(() => useQuiz(), { wrapper });
-    act(() => {
-      result.current.setQuizzes(mockQuizzes);
-    });
+    startQuiz(result);
+    expect(result.current.quizzes).toEqual(mockQuizzes);
   });
 
-  test("퀴즈 배열이 초기화 됐는지 확인한다.", async () => {
+  test("처음 퀴즈를 시작하면 퀴즈를 다 풀지 않은 것이다.", () => {
     const { result } = renderHook(() => useQuiz(), { wrapper });
-    expect(result.current.quizzes).toEqual(mockQuizzes);
+    expect(result.current.isFinished).toEqual(false);
+  });
+
+  test("마지막 퀴즈를 풀고 다음으로 넘어가면 퀴즈를 다 푼 것이다.", () => {
+    const { result } = renderHook(() => useQuiz(), { wrapper });
+    startQuiz(result);
+    mockQuizzes.forEach(() => {
+      act(() => {
+        result.current.goNextQuiz();
+      });
+    });
+    expect(result.current.isFinished).toEqual(true);
+  });
+
+  test("틀린 문제 세기", () => {
+    const { userAnswers, wrongCount } = makeRandomUserAnswers(mockQuizzes.length);
+    const { result } = renderHook(() => useQuiz(), { wrapper });
+    startQuiz(result);
+    userAnswers.forEach((answer) => {
+      act(() => {
+        result.current.selectAnswer(answer);
+        result.current.goNextQuiz();
+      });
+    });
+    expect(result.current.wrongQuizzesCount).toEqual(wrongCount);
+  });
+
+  test("맞은 문제 세기", () => {
+    const { userAnswers, correctCount } = makeRandomUserAnswers(mockQuizzes.length);
+    const { result } = renderHook(() => useQuiz(), { wrapper });
+    startQuiz(result);
+    userAnswers.forEach((answer) => {
+      act(() => {
+        result.current.selectAnswer(answer);
+        result.current.goNextQuiz();
+      });
+    });
+    expect(result.current.correctQuizzesCount).toEqual(correctCount);
+  });
+
+  test("오답인 퀴즈들 가져오기", () => {
+    const { userAnswers } = makeRandomUserAnswers(mockQuizzes.length);
+    const { result } = renderHook(() => useQuiz(), { wrapper });
+    startQuiz(result);
+    userAnswers.forEach((answer) => {
+      act(() => {
+        result.current.selectAnswer(answer);
+        result.current.goNextQuiz();
+      });
+    });
+
+    expect(result.current.wrongQuizzes).toEqual(getSortedWrongQuizzes(userAnswers));
+  });
+
+  test("풀고 있는 문제가 정답인지 아닌지 확인한다.", () => {
+    const answers = getQuestions();
+    const { result } = renderHook(() => useQuiz(), { wrapper });
+    startQuiz(result);
+    answers.forEach(({ correctAnswer, wrongAnswers }) => {
+      act(() => {
+        wrongAnswers.forEach((wrongAnswer) => {
+          expect(result.current.isCorrect(wrongAnswer)).toEqual(false);
+        });
+        expect(result.current.isCorrect(correctAnswer)).toEqual(true);
+        result.current.selectAnswer(true);
+        result.current.goNextQuiz();
+      });
+    });
   });
 });
