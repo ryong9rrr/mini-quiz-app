9cdf2e3e045531569a3c2f6bbb60f8b8d43c83fd
yong <tkddbs1644@gmail.com>
2022-12-07 21:11:16 +0900

    test(quizContext): 주요 기능 테스트 코드 추가

---
+++ src/modules/contexts/quiz.test.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "@testing-library/jest-dom";
+import { act, renderHook } from "@testing-library/react";
+import { mockQuizzes } from "~/testing/mock";
+import { QuizContextProvider, useQuiz } from "./quiz";
+
+const wrapper = ({ children }: { children: React.ReactNode }) => (
+  <QuizContextProvider hasStorage={false}>{children}</QuizContextProvider>
+);
+
+const startQuiz = (result: { current: ReturnType<typeof useQuiz> }) => {
+  act(() => {
+    result.current.setNewQuizzes(mockQuizzes);
+  });
+};
+
+describe("useQuiz 기능 테스트", () => {
+  test("새로운 퀴즈를 시작한다.", () => {
+    const { result } = renderHook(() => useQuiz(), { wrapper });
+    startQuiz(result);
+    expect(result.current.quizzes).toEqual(mockQuizzes);
+  });
+
+  test("다음 퀴즈로 넘어간다.", () => {
+    const { result } = renderHook(() => useQuiz(), { wrapper });
+    startQuiz(result);
+    act(() => {
+      result.current.goNextQuiz(true);
+    });
+    expect(result.current.currentQuizIndex).toEqual(1);
+  });
+
+  test("틀린 문제 개수를 반환한다.", () => {
+    const { result } = renderHook(() => useQuiz(), { wrapper });
+    startQuiz(result);
+    act(() => {
+      result.current.goNextQuiz(false);
+    });
+    const wrongCount = result.current.wrongQuizIndexNumbers.length;
+    expect(wrongCount).toEqual(1);
+  });
+});
