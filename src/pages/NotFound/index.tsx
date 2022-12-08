775d75a4c9aa0049ac2cf575cb596d9a33d487d1
yong <tkddbs1644@gmail.com>
2022-12-09 04:03:59 +0900

    design(404): 404 페이지 스타일링

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,7 +1,43 @@
 import React from "react";
+import Lottie from "react-lottie";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { Button, Text } from "~/components/atom";
+import data from "~/lib/data/notFound.json";
 
 const NotFoundPage = () => {
-  return <h1>404 Not Found</h1>;
+  const navigate = useNavigate();
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: data,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  const handleClickButton = () => {
+    navigate("/");
+  };
+
+  return (
+    <div>
+      <Lottie options={defaultOptions} height={400} width={400} />
+      <Texts>
+        <Text size="lg">존재하지 않는 페이지입니다.</Text>
+        <Button onClick={handleClickButton}>홈으로</Button>
+      </Texts>
+    </div>
+  );
 };
 
 export default NotFoundPage;
+
+const Texts = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 16px;
+`;
